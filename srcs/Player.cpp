ced3885d8c95e5612a63c2dcdb6519b4d4171fb8
Enguerrand Arminjon <enguerrand.Arminjon@epitech.eu>
2018-05-16 13:11:58 +0200

    merge

--- srcs/Player.cpp
+++ srcs/Player.cpp
@@ -31,8 +31,6 @@ Indie::Player::~Player() = default;
  */
 void Indie::Player::move(const Events &event)
 {
-	irr::core::vector3df lastP = _player->getPosition();
-	irr::core::vector3df lastR = _player->getRotation();
 	irr::core::vector3df nodePosition = _player->getPosition();
 
 	if (event.isKeyDown(irr::KEY_KEY_Q)) {
