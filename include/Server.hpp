ff6e754f189400d9527a505db40354d523b162ab
Enguerrand Arminjon <enguerrand.Arminjon@epitech.eu>
2018-05-25 15:46:38 +0200

    seg need fix

---
+++ include/Server.hpp
@@ -0,0 +1,56 @@
+//
+// EPITECH PROJECT, 2018
+// cpp_indie_studio
+// File description:
+// Server
+//
+
+#pragma once
+
+#include <memory>
+#include <algorithm>
+#include "Socket.hpp"
+
+namespace Indie {
+	enum GameState {
+		NOTCONNECTED,
+		WAITING,
+		PLAYING
+	};
+
+	class Client {
+		public:
+			Client(int id, int fd, const std::string &name) : _id(id), _fd(fd), _name(name), _state(WAITING) {}
+			int _id;
+			int _fd;
+			std::string _name;
+			GameState _state;
+	};
+
+	bool operator==(std::unique_ptr<Client> &, std::unique_ptr<Client> &);
+	bool operator!=(std::unique_ptr<Client> &, std::unique_ptr<Client> &);
+
+	class Server {
+		public:
+			Server();
+			void start();
+			void setClientsFds();
+			void readOnFds();
+			int maxFd() const;
+			void addClient();
+			int readClient(std::unique_ptr<Client> &);
+			GameState checkIfStartGame();
+			static void runServer();
+		private:
+			fd_set _fdRead;
+
+			Socket _socket;
+			int _hostFd;
+			GameState _state;
+			std::vector<std::unique_ptr<Client>> _clients;
+			std::vector<std::vector<int>> _map;
+			std::string _mapMsg;
+	};
+
+}
+
