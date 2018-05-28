2bc485c4d65e0ef6dcaee6a2339d3c27fc8b0939
Benjamin GAYMAY <benjamin.gaymay@epitech.eu>
2018-05-28 16:56:05 +0200

    parsing of server informations

--- srcs/Client/ConnectionWithServer.cpp
+++ srcs/Client/ConnectionWithServer.cpp
@@ -9,11 +9,45 @@
 #include <Player.hpp>
 #include "ManageStrings.hpp"
 
-void Indie::Core::addPlayer(int id, irr::core::vector2di &pos2d, irr::core::vector3df &pos, const irr::f32 &rota)
+void Indie::Core::comGameInfos(int event, std::vector<std::string> &infos)
 {
-	std::cout << "Add player: " << id << std::endl;
+	switch (event) {
+		case START:
+			_state = PLAYING;
+			m_state = PLAY;
+			_playerObjects.insert(_playerObjects.begin(), std::make_unique<Player>(_playerId, _graphism->createTexture(*_graphism->getTexture(10), {0, _mapper->getHeight(), 0}, {0, 0, 0}, {2, 2, 2}, true), _tchat));
+			_graphism->resizeNode(_playerObjects[0]->getPlayer(), _mapper->getSize());
+			m_core.getCamera().change(m_core.getSceneManager());
+			_graphism->buildDecor();
+			break;
+		case MESSAGE: serverMessage(infos); break;
+	}
+}
+
+void Indie::Core::comMap(int event, std::vector<std::string> &infos)
+{
+	switch (event) {
+		case APPEAR: _mapper = std::make_unique<Map>(infos, 20.0f, 100.0f, _graphism); break;
+	}
+}
+
+void Indie::Core::comPlayer(int event, std::vector<std::string> &infos)
+{
+	try {
+		auto id = std::stoi(infos[0]);
+
+		switch (event) {
+			case DEAD: removePlayer(id); break;
+			case APPEAR: addPlayer(id, irr::core::vector2di(stoi(infos[1]), std::stoi(infos[2])), irr::core::vector3df(std::stof(infos[3]), std::stof(infos[4]), std::stof(infos[5])), std::stof(infos[6])); break;
+			case MOVE: movePlayer(id, irr::core::vector2di(stoi(infos[1]), std::stoi(infos[2])), irr::core::vector3df(std::stof(infos[3]), std::stof(infos[4]), std::stof(infos[5])), std::stof(infos[6])); break;
+		}
+	} catch (const std::exception &e) {}
+}
+
+void Indie::Core::addPlayer(int id, const irr::core::vector2di &pos2d, const irr::core::vector3df &pos3d, const irr::f32 &rota)
+{
 	std::unique_ptr<Player> newPlayer = std::make_unique<Player>(id, _graphism->createTexture(
-					*_graphism->getTexture(10), pos, {0, 0, 0}, {2, 2, 2}, true), _tchat);
+					*_graphism->getTexture(10), pos3d, {0, 0, 0}, {2, 2, 2}, true), _tchat);
 	_graphism->resizeNode(newPlayer->getPlayer(), _mapper->getSize());
 	newPlayer->setSpeed(1);
 	newPlayer->getPlayer()->setRotation({0, rota, 0});
@@ -21,11 +55,8 @@ void Indie::Core::addPlayer(int id, irr::core::vector2di &pos2d, irr::core::vect
 	_playerObjects.push_back(std::move(newPlayer));
 }
 
-void Indie::Core::removePlayer(int id, irr::core::vector2di &pos2d, irr::core::vector3df &pos, const irr::f32 &rota)
+void Indie::Core::removePlayer(int id)
 {
-	(void) pos;
-	(void) rota;
-	(void) pos2d;
 	if (id == _playerObjects[0]->getId())
 		return;
 	for (auto &p : _playerObjects) {
@@ -38,7 +69,7 @@ void Indie::Core::removePlayer(int id, irr::core::vector2di &pos2d, irr::core::v
 	}
 }
 
-void Indie::Core::movePlayer(int id, irr::core::vector2di &pos2d, irr::core::vector3df &pos, const irr::f32 &rota)
+void Indie::Core::movePlayer(int id, const irr::core::vector2di &pos2d, const irr::core::vector3df &pos, const irr::f32 &rota)
 {
 	for (auto &p : _playerObjects)
 		if (p->getId() == id) {
@@ -67,46 +98,16 @@ void Indie::Core::serverMessage(const std::vector<std::string> &message)
 
 void Indie::Core::readServerInformations(std::vector<std::string> servSend)
 {
-	std::vector<std::string> info;
-	int type, event, id;
-	irr::f32 rota;
+	std::vector<std::string> infos;
+	int type, event;
 
 	for (auto &line : servSend) {
-		info = ManageStrings::splitString(line, ':');
-		if (!info.empty() && info.size() >= 2) { // bha oui, si info[1] exist pas, segfault...
-			if (ManageStrings::isInteger(info[0]) && ManageStrings::isInteger(info[1])) {
-				type = std::stoi(info[0]);
-				event = std::stoi(info[1]);
-				info.erase(info.begin(), info.begin() + 2);
-				if (type == GAMEINFOS && event == START) {
-					_state = PLAYING;
-					m_state = PLAY;
-					_playerObjects.insert(_playerObjects.begin(), std::make_unique<Player>(_playerId, _graphism->createTexture(*_graphism->getTexture(10), {0, _mapper->getHeight(), 0}, {0, 0, 0}, {2, 2, 2}, true), _tchat));
-					_graphism->resizeNode(_playerObjects[0]->getPlayer(), _mapper->getSize());
-					m_core.getCamera().change(m_core.getSceneManager());
-					_graphism->buildDecor();
-				} else if (type == GAMEINFOS && event == MESSAGE) {
-					serverMessage(info);
-				} else if (type == MAP and event == APPEAR) {
-					std::cout << "On recois la carte\n";
-					_mapper = std::make_unique<Map>(info, 20.0f, 100.0f, _graphism);
-				} else {
-					id = std::stoi(info[0]);
-
-					irr::core::vector2di pos2d(stoi(info[1]), std::stoi(info[2]));
-					irr::core::vector3df pos3d(std::stof(info[3]), std::stof(info[4]), std::stof(info[5]));
-					if (type == PLAYER && event == MOVE) {
-						rota = std::stof(info[6]); // pas tout le monde l'utilise
-						(this->*_playersFct[event])(id, pos2d, pos3d, rota);
-					} /*else if (type == PLAYER && event == DROPBOMB)
-						type = type;*/
-					/*switch (type) {
-						case Indie::PLAYER:
-							(this->*_playersFct[event])(id, pos2d, pos3d, rota); break;
-						default:break;
-					}*/
-				}
-			}
+		infos = ManageStrings::splitString(line, ':');
+		if (infos.size() >= 2 && ManageStrings::isInteger(infos[0]) && ManageStrings::isInteger(infos[1])) {
+			type = std::stoi(infos[0]);
+			event = std::stoi(infos[1]);
+			infos.erase(infos.begin(), infos.begin() + 2);
+			(this->*_objectsFct[type])(event, infos);
 		}
 	}
 }
