10085f208de7606d637e61974f267802b577f725
Benjamin GAYMAY <benjamin.gaymay@epitech.eu>
2018-06-07 12:17:43 +0200

    clean

--- srcs/Client/Core/Core.cpp
+++ srcs/Client/Core/Core.cpp
@@ -42,17 +42,16 @@ void Indie::Core::drawCaption()
 void Indie::Core::processEvents()
 {
 	if (_chat._getch)
-		_chat.manageTchat(m_event, _socket);
-	else {
-		if (m_event.isKeyDown(irr::KEY_ESCAPE)) {
-			m_event.setKeyUp(irr::KEY_ESCAPE);
-			if (m_state == PLAY or m_state == SPEC) {
-				m_core.m_device->getCursorControl()->setVisible(true);
-				m_core.getCamera().change(m_core.getSceneManager(), Camera::BASIC);
-				m_menu.m_gameOptions->setVisible(true);
-			}
+		return _chat.manageTchat(m_event, _socket);
+	if (m_event.isKeyDown(irr::KEY_ESCAPE)) {
+		m_event.setKeyUp(irr::KEY_ESCAPE);
+		if (m_state == PLAY or m_state == SPEC) {
+			m_core.m_device->getCursorControl()->setVisible(true);
+			m_core.getCamera().change(m_core.getSceneManager(), Camera::BASIC);
+			m_menu.m_gameOptions->setVisible(true);
 		}
-		if (m_event.isKeyDown(irr::KEY_RETURN)) {
+	}
+	if (m_event.isKeyDown(irr::KEY_RETURN)) {
 			m_event.setKeyUp(irr::KEY_RETURN);
 			if (_socket) {
 				_chat._getch = true;
@@ -60,7 +59,6 @@ void Indie::Core::processEvents()
 				m_core.m_gui->setFocus(_chat._textBox);
 				_chat._textBox->setText(L"");
 			}
-		}
 	}
 	m_evtManager->manage(_engine);
 }
@@ -75,14 +73,14 @@ void Indie::Core::checkAppContext()
 	old = m_state;
 	if (m_state == LAUNCH_SERVER && _state == NOTCONNECTED) {
 		std::thread(&Indie::Server::runServer).detach();
-		_state = WAITING;
-		oldMap = "";
-		while (1) {
+		while (true) {
 			try {
 				_socket = std::make_unique<Socket>(5567, "127.0.0.1", Socket::CLIENT);
 				break;
 			} catch (const std::exception &e) {}
 		}
+		_state = WAITING;
+		oldMap = "";
 		_playerId = waitForId();
 	}
 	if (m_state == READY && _state == WAITING) {
@@ -123,9 +121,8 @@ void Indie::Core::checkAppContext()
 void Indie::Core::exitGame()
 {
 	// Y A DES TRUCS QUI SE DELETE PAS (lancer deux joueurs / quitter le serveur / lancer un serveur sur le second et jouer)
-	if (_state != NOTCONNECTED) {
+	if (_state != NOTCONNECTED)
 		dprintf(_socket->getFd(), "%d:%d:%d\n", PLAYER, LEAVE, _playerObjects[0]->getId());
-	}
 	_readyPlayers.clear();
 	_mapper->clear3dMap();
 	_mapper.release();
@@ -145,11 +142,7 @@ void Indie::Core::exitGame()
 void Indie::Core::run()
 {
 	irrklang::ISound* music = _engine->play2D("music/main.wav", true, false, true);
-	if (m_opts.getMusic())
-		music->setVolume(0.3);
-	else
-		music->setVolume(0);
-
+	music->setVolume(m_opts.getMusic() ? 0.3 : 0);
 	if (m_opts.getSplashScreen())
 		m_splash.display(m_core.m_device, m_event);
 	m_menu.loadMenu(m_core.m_device, m_opts);
@@ -169,8 +162,7 @@ void Indie::Core::run()
 		m_core.m_driver->endScene();
 		drawCaption();
 	}
-
-	music->drop(); // release music stream.
+	music->drop();
 }
 
 void Indie::Core::infoReadyPlayerOne()
