172235a2edf0537055251d38c721b92ef6a1d210
Benoit PIngris <benoit.pingris@epitech.eu>
2018-06-07 11:17:30 +0200

    change position of menu

--- srcs/Client/GUI/Menu.cpp
+++ srcs/Client/GUI/Menu.cpp
@@ -117,27 +117,27 @@ void Indie::Menu::loadMainMenu()
 {
 	auto image = m_driver->getTexture ("assets/models/menu/background.png");
 
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft,200,m_xRight,200 + m_height), m_main, GUI_ID_PLAY_BUTTON,
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft,220,m_xRight,220 + m_height), m_main, GUI_ID_PLAY_BUTTON,
             L"Play", L"Launches the game"));
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft,310,m_xRight,310 + m_height), m_main, GUI_ID_MAP_BUTTON,
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft,330,m_xRight,330 + m_height), m_main, GUI_ID_MAP_BUTTON,
             L"Map Editor", L"Create your own maps"));
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft,420,m_xRight,420 + m_height), m_main, GUI_ID_OPTIONS_BUTTON,
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft,440,m_xRight,440 + m_height), m_main, GUI_ID_OPTIONS_BUTTON,
             L"Option", L"Changes options"));
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft,530,m_xRight,530 + m_height), m_main, GUI_ID_QUIT_BUTTON,
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft,550,m_xRight,550 + m_height), m_main, GUI_ID_QUIT_BUTTON,
             L"Quit", L"Exits the program"));
 	m_gui->addImage(image, irr::core::vector2di(0, 0), true, m_main);
 }
 
 void Indie::Menu::loadReadyMenu()
 {
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 325, m_xRight, 325 + m_height), m_ready, GUI_ID_UNREADY, L"NOT READY"));
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 330, m_xRight, 330 + m_height), m_ready, GUI_ID_UNREADY, L"NOT READY"));
 }
 
 void Indie::Menu::loadOptionsMenu()
 {
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 325, m_xRight, 325 + m_height), m_option, GUI_ID_OPTION_BACK_BUTTON,
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 330, m_xRight, 330 + m_height), m_option, GUI_ID_OPTION_BACK_BUTTON,
 		L"Back"));
-	m_gui->addCheckBox(m_opt.getFullScreen(), irr::core::recti(m_width, m_height, m_width + 250, m_height + 50), m_option, GUI_ID_FULLSCREEN, L"FullScreen");
+	m_gui->addCheckBox(m_opt.getFullScreen(), irr::core::recti(m_width + 10, m_height, m_width + 250, m_height + 50), m_option, GUI_ID_FULLSCREEN, L"FullScreen");
 }
 
 void Indie::Menu::loadJoinMenu()
@@ -145,9 +145,9 @@ void Indie::Menu::loadJoinMenu()
 	auto edit = m_gui->addEditBox(L"127.0.0.1", irr::core::rect<irr::s32>(m_xLeft,90,m_xRight,90 + m_height - 10), true, m_join, GUI_ID_IP);
 	edit->setMax(15);
 	edit->setOverrideColor(irr::video::SColor(255, 0, 0, 255));
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 215, m_xRight, 215 + m_height), m_join, GUI_ID_JOIN_BUTTON,
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 220, m_xRight, 220 + m_height), m_join, GUI_ID_JOIN_BUTTON,
 		L"Join"));
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 325, m_xRight, 325 + m_height), m_join, GUI_ID_JOIN_BACK_BUTTON,
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 330, m_xRight, 330 + m_height), m_join, GUI_ID_JOIN_BACK_BUTTON,
 		L"Back"));
 
 	{
@@ -174,7 +174,7 @@ void Indie::Menu::loadJoinMenu()
 void Indie::Menu::loadDownMenu()
 {
 	m_gui->addStaticText(L"Server is down", irr::core::rect<irr::s32>(m_xLeft + 20, 250, m_xRight + 50, 250 + m_height), false, true, m_down);
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 435, m_xRight, 435 + m_height), m_down, GUI_ID_DOWN_BUTTON, L"Back to menu"));
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 440, m_xRight, 440 + m_height), m_down, GUI_ID_DOWN_BUTTON, L"Back to menu"));
 }
 
 void Indie::Menu::loadMapMenu()
@@ -182,11 +182,11 @@ void Indie::Menu::loadMapMenu()
 	auto edit = m_gui->addEditBox(L"Map's name", irr::core::rect<irr::s32>(m_xLeft,90,m_xRight,90 + m_height - 10), true, m_mapMenu, GUI_ID_MAP_NAME);
 	edit->setMax(10);
 	edit->setOverrideColor(irr::video::SColor(255, 0, 0, 255));
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 215, m_xRight, 215 + m_height), m_mapMenu, GUI_ID_MAP_EDITOR_BUTTON,
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 220, m_xRight, 220 + m_height), m_mapMenu, GUI_ID_MAP_EDITOR_BUTTON,
 		L"Map Editor"));
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 325, m_xRight, 325 + m_height), m_mapMenu, GUI_ID_MAP_RANDOM_BUTTON,
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 330, m_xRight, 330 + m_height), m_mapMenu, GUI_ID_MAP_RANDOM_BUTTON,
 		L"Random map"));
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 435, m_xRight, 435 + m_height), m_mapMenu, GUI_ID_MAP_BACK_BUTTON,
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 440, m_xRight, 440 + m_height), m_mapMenu, GUI_ID_MAP_BACK_BUTTON,
 		L"Back"));
 	m_small_btns.emplace_back(m_gui->addButton(irr::core::recti(10, m_opt.getHeight() / 2 - m_small_height / 2 , m_small_width, m_opt.getHeight() / 2 + m_small_height / 2 ), m_mapEdit, GUI_ID_MAP_SAVE_BUTTON,
 		L"Save"));
@@ -194,10 +194,10 @@ void Indie::Menu::loadMapMenu()
 
 void Indie::Menu::loadPlayMenu()
 {
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 105, m_xRight, 105 + m_height), m_play, GUI_ID_LOCAL, L"LOCAL"));
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 215, m_xRight, 215 + m_height), m_play, GUI_ID_PLAY_SERVER, L"CREATE ROOM"));
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 325, m_xRight, 325 + m_height), m_play, GUI_ID_PLAY_CLIENT, L"JOIN ROOM"));
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 435, m_xRight, 435 + m_height), m_play, GUI_ID_PLAY_BACK_BUTTON,
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 220, m_xRight, 220 + m_height), m_play, GUI_ID_LOCAL, L"LOCAL"));
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 330, m_xRight, 330 + m_height), m_play, GUI_ID_PLAY_SERVER, L"CREATE ROOM"));
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 440, m_xRight, 440 + m_height), m_play, GUI_ID_PLAY_CLIENT, L"JOIN ROOM"));
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 550, m_xRight, 550 + m_height), m_play, GUI_ID_PLAY_BACK_BUTTON,
 		L"Back"));
 }
 
@@ -220,8 +220,8 @@ void Indie::Menu::chooseMap(irr::s32 id)
 
 void Indie::Menu::loadRoomSMenu()
 {
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 215, m_xRight, 215 + m_height), m_roomS, GUI_ID_READY, L"READY"));
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 325, m_xRight, 325 + m_height), m_roomS, GUI_ID_ROOM_BACK_BUTTON,
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 220, m_xRight, 220 + m_height), m_roomS, GUI_ID_READY, L"READY"));
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 330, m_xRight, 330 + m_height), m_roomS, GUI_ID_ROOM_BACK_BUTTON,
 		L"Back"));
 
 	m_gui->addStaticText(L"Select your map:", irr::core::recti(100, 20, 800, 20 + m_height), false, true, m_roomS)->setOverrideColor(irr::video::SColor(150, 255, 0, 255));
@@ -231,16 +231,16 @@ void Indie::Menu::loadRoomSMenu()
 
 void Indie::Menu::loadRoomCMenu()
 {
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 215, m_xRight, 215 + m_height), m_roomC, GUI_ID_READY, L"READY"));
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 325, m_xRight, 325 + m_height), m_roomC, GUI_ID_ROOM_BACK_BUTTON,
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 220, m_xRight, 220 + m_height), m_roomC, GUI_ID_READY, L"READY"));
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 330, m_xRight, 330 + m_height), m_roomC, GUI_ID_ROOM_BACK_BUTTON,
 		L"Back"));
 }
 
 void Indie::Menu::loadGameOptionsMenu()
 {
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 215, m_xRight, 215 + m_height), m_gameOptions, GUI_ID_STAY_GAME_BUTTON, L"CONTINUE"));
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 325, m_xRight, 325 + m_height), m_gameOptions, GUI_ID_LEAVE_GAME_BUTTON, L"LEAVE ROOM"));
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 435, m_xRight, 435 + m_height), m_gameOptions, GUI_ID_QUIT_GAME_BUTTON, L"QUIT GAME"));
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 220, m_xRight, 220 + m_height), m_gameOptions, GUI_ID_STAY_GAME_BUTTON, L"CONTINUE"));
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 330, m_xRight, 330 + m_height), m_gameOptions, GUI_ID_LEAVE_GAME_BUTTON, L"LEAVE ROOM"));
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 440, m_xRight, 440 + m_height), m_gameOptions, GUI_ID_QUIT_GAME_BUTTON, L"QUIT GAME"));
 
 }
 
@@ -255,8 +255,8 @@ void Indie::Menu::loadSoundBtn()
 
 void Indie::Menu::loadLocalGame()
 {
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 215, m_xRight, 215 + m_height), m_local, GUI_ID_PLAY_LOCAL, L"Play"));
-	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 325, m_xRight, 325 + m_height), m_local, GUI_ID_LOCAL_BACK, L"Back"));
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 220, m_xRight, 220 + m_height), m_local, GUI_ID_PLAY_LOCAL, L"Play"));
+	m_btns.emplace_back(m_gui->addButton(irr::core::recti(m_xLeft, 330, m_xRight, 330 + m_height), m_local, GUI_ID_LOCAL_BACK, L"Back"));
 	m_gui->addStaticText(L"Select your map:", irr::core::recti(100, 20, 800, 20 + m_height), false, true, m_local)
 			->setOverrideColor(irr::video::SColor(150, 255, 0, 255));
 	m_gui->addListBox(irr::core::recti(100, 100, 400, m_opt.getHeight() - 100), m_local, GUI_ID_LOCAL_MAPS);
